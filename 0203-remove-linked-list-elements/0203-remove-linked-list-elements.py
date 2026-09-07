cb1d20383b855203af80b9fb64159660d68cfe15
Debabrata Dey <debabratadey8080@gmail.com>
2026-09-07 08:36:59 +0530

    Time: 3 ms (72.92%), Space: 19.3 MB (61.45%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        current = head
+        previous = None
+
+        while current is not None:
+
+            if current.val == val:
+
+                if previous is None:
+                    head = current.next
+                else:
+                    previous.next=current.next
+
+            else:
+                previous=current
+
+            current=current.next
+
+        return head
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
